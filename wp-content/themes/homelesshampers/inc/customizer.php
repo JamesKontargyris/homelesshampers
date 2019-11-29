9983dce289c4d81f1d42fb42ca3837370614f6d3
James Kontargyris <james@jameskontargyris.co.uk>
2019-11-29 16:28:21 +0000

    Initial commit

---
+++ wp-content/themes/homelesshampers/inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * homelesshampers Theme Customizer
+ *
+ * @package homelesshampers
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function homelesshampers_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'homelesshampers_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'homelesshampers_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'homelesshampers_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function homelesshampers_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function homelesshampers_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function homelesshampers_customize_preview_js() {
+	wp_enqueue_script( 'homelesshampers-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'homelesshampers_customize_preview_js' );
