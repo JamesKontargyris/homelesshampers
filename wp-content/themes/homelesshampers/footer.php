4037c33b5b99fa5745935077fabd665171c7c1aa
James Kontargyris <james@jameskontargyris.co.uk>
2019-12-09 22:52:02 +0000

    Added footer

--- wp-content/themes/homelesshampers/footer.php
+++ wp-content/themes/homelesshampers/footer.php
@@ -11,23 +11,58 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'homelesshampers' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'homelesshampers' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'homelesshampers' ), 'homelesshampers', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer class="site__footer">
+
+    <div class="site__footer__social-media-donate-container">
+        <div class="site__footer__social-media-donate-wrapper">
+
+            <div class="site__footer__social-media">
+                <div class="site__footer__social-media__facebook">
+                    <div class="site__footer__social-media__title site__footer__social-media__title--facebook">Facebook Likes</div>
+                    <p class="site__footer__social-media__number">6,458</p>
+                    <a href="#" class="btn btn--secondary">Like our page</a>
+                </div>
+                <div class="site__footer__social-media__twitter">
+                    <div class="site__footer__social-media__title site__footer__social-media__title--twitter">Twitter Followers</div>
+                    <p class="site__footer__social-media__number">199</p>
+                    <a href="#" class="btn btn--secondary">Follow us</a>
+                </div>
+
+            </div>
+            <div class="site__footer__donate-box">
+                <h5>Donate today</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                <form action="/" method="POST" class="site__footer__donate-box__form">
+                    <input type="text" name="amount" placeholder="Enter amount...">
+                    <button type="submit">Donate &gt;</button>
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="site__footer__legal-container">
+        <div class="site__footer__legal">
+
+            <ul class="site__footer__legal__menu">
+                <li><a href="#">Our Mission</a></li>
+                <li><a href="#">Supporters</a></li>
+                <li><a href="#">Our Team</a></li>
+                <li><a href="#">Get Involved</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+
+            <div class="site__footer__legal__copyright">
+                &copy; Homeless Hampers 2019. All rights reserved.<br>Registered charity number 12345678. <a
+                        href="#">Privacy policy</a>
+            </div>
+
+        </div>
+    </div>
+
+</footer><!-- .site__footer -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
